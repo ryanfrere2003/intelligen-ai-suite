3de43a4ff8505aba65fb58e566ca653b6183fc9f
notacat1221 <111917734+notacat1221@users.noreply.github.com>
2026-08-05 14:36:17 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,21 +1,24 @@
 """ has configuration data for the application including commonly used paths"""
-
-import os, json
 from pathlib import Path
+import os, json
 from dotenv import load_dotenv
 
 load_dotenv()
 
-USER_FULL_NAME = os.getenv("USER_FULL_NAME")
+USER_FIRST_NAME = os.getenv("USER_FIRST_NAME")
+USER_LAST_NAME = os.getenv("USER_LAST_NAME")
+USER_MIDDLE_NAMES= json.loads(os.getenv("USER_MIDDLE_NAMES", "[]"))
+
 USER_EMAIL = os.getenv("USER_EMAIL")
-USER_USERNAME = os.getenv("USER_USERNAME")
-USER_LOCATIONS = json.loads(
-    os.getenv("USER_LOCATIONS", "[]")
-)
+USER_USERNAME = json.loads(os.getenv("USER_USERNAMES", "[]"))
+USER_LOCATIONS = json.loads(os.getenv("USER_LOCATIONS", "[]"))
 
 
 #--------------
 # PATHS
 #--------------
 #TODO: you will need to provide your own mbox file
-MAILBOX_PATH = "data/raw/raw.mbox"
+PROJECT_ROOT = Path(__file__).resolve().parent
+
+DATABASE_PATH = PROJECT_ROOT / "database" / "main.db"
+MAILBOX_PATH = PROJECT_ROOT / "data" / "raw" / "raw.mbox"
